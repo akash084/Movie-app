b59a1680d150ecf1d3b0ce3287672f69aafb4d71
akash084 <akashbaniya084@gmail.com>
2025-09-26 14:47:26 +1000

    second commit

--- src/App.jsx
+++ src/App.jsx
@@ -47,7 +47,7 @@ const App = () => {
 				throw new Error("Failed to fetch movies");
 			}
 			const data = await response.json();
-			console.log(data);
+			// console.log(data);
 
 			if (data.Response === "False") {
 				setErrorMessage(data.Error || "Failed to fetch movies");
